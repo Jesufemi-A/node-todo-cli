ecccfe6548021b0f045861b15d952d300fdbde31
Jesufemi-A <jesufemia1@gmail.com>
2025-10-06 15:06:52 +0200

    implemented markTask functionality

--- todo.js
+++ todo.js
@@ -1,14 +1,18 @@
+const { fileURLToPath } = require("url");
 const taskManager = require("./taskManager");
 const fs = require("fs");
 
 filePath = "./tasks.json";
 let command = process.argv[2];
-let title = process.argv[3];
 
 if (command === "add") {
-  taskManager.addTask(filePath, title);
+  taskManager.addTask(filePath, process.argv[3]);
 }
 
 if (command === "list") {
   taskManager.listTask(filePath);
+}
+
+if (command === "mark") {
+  taskManager.markTask(filePath, parseInt(process.argv[3]));
 }
