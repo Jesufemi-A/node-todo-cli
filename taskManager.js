b388b2e2974c7263e2183fd3d73cd4c2ab16bc40
Jesufemi-A <jesufemia1@gmail.com>
2025-10-06 09:33:40 +0200

    added addTask function and handled if the app is running for the first time. Still debugging for if the app isnt running for the time

--- taskManager.js
+++ taskManager.js
@@ -1,7 +1,30 @@
 const fs = require("fs");
 
-function addTask(title, taskArr) {
-  task = { title: title };
+exports.addTask = (filePath, title) => {
+  // If tasks.json file doesn't exist and app is running for the first time
+  try {
+    if (!fs.existsSync(filePath)) {
+      let task = [
+        {
+          title: title,
+          status: "pending",
+        },
+      ];
+      task = JSON.stringify(task);
+      fs.writeFileSync("./tasks.json", task);
 
-  taskArr.push(task);
-}
+      // If tasks.json already existed and app isn't running for the first time
+    } else {
+      let tasks = JSON.parse(fs.readFileSync(filePath, "utf-8"));
+      const task = JSON.stringify({
+        title: title,
+        status: "pending",
+      });
+      tasks.push(task);
+      fs.writeFileSync(filePath, tasks);
+    }
+    console.log("Task added successfully");
+  } catch (err) {
+    console.log("Can not add file");
+  }
+};
